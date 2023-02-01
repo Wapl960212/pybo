facbfc78f7c05b8d8c6e10737b3e04ba6f73ea08
Wapl960212 <leo_960212@naver.com>
2023-02-01 11:28:54 +0900

    django form 사용.1

--- pybo/foms.py
+++ pybo/foms.py
@@ -14,10 +14,7 @@ class QuestionForm(forms.ModelForm):
         model =Question
 
         fields = ['subject','content']
-        widgets = {
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control','rows':10}),
-        }
+
         labels ={
             'subject':'제목',
             'content':'내용',
